9f9914c1d18ad59709d40ca78b5a52dc6b5e6e2d
marshuniverse <1261565515@outlook.com>
2023-05-07 10:56:06 +0800

    修复到小分辨率情况下图像的页边距过大(10)的问题

--- Prototype/text2pic.py
+++ Prototype/text2pic.py
@@ -1,4 +1,3 @@
-
 from PIL import Image, ImageFont, ImageDraw
 
 def TextSplit(text, length):
@@ -8,19 +7,19 @@ def Text2Pic(raw,width,height,size) -> Image:
 
     sections = raw.split(sep='\n')
     text = ""
+    margin = 10 if width>64 else 2
     for section in sections:
-        for line in TextSplit(section,int((width-20)/size)):
+        for line in TextSplit(section,int((width-margin*2)/size)):
             text += line+'\n'
     
     image = Image.new("1", (width, height), 1)
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype("./YaHeiConsolas.ttf", size)
 
-    draw.multiline_text((10, 5), text, font=font, fill="#000000")
+    draw.multiline_text((margin, margin/2), text, font=font, fill="#000000")
     return image
 
 text = u"　　These is a sample text.\n这几天心里颇不宁静。今晚在院子里坐着乘凉，忽然想起日日走过的荷塘，在这满月的光里，总该另有一番样子吧。\n　　月亮渐渐地升高了，墙外马路上孩子们的欢笑，已经听不见了；妻在屋里拍着闰儿，迷迷糊糊地哼着眠歌。"
 img = Text2Pic(text,512,512,36)
-img.show()
-img.save("test.png")
+img.save("./Test/txt2pic.png")
 
--- text2pic.py
+++ text2pic.py
@@ -7,14 +7,15 @@ class Converter:
     def Text2Pic(self,raw,width,height,size) -> Image:
         sections = raw.split(sep='\n')
         text = ""
+        margin = 10 if width>64 else 2
         for section in sections:
-            for line in self.TextSplit(section,int((width-20)/size)):
+            for line in self.TextSplit(section,int((width-margin*2)/size)):
                 text += line+'\n'
         
         image = Image.new("1", (width, height), 1)
         draw = ImageDraw.Draw(image)
         font = ImageFont.truetype("./YaHeiConsolas.ttf", size)
 
-        draw.multiline_text((10, 5), text, font=font, fill="#000000")
+        draw.multiline_text((margin, margin/2), text, font=font, fill="#000000")
         return image
 
